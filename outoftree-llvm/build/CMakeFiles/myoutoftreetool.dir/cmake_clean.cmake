a64234854ebc689ea9a31614182eb86152e961bb
Sameeran joshi <joshisameeran17@gmail.com>
2021-05-31 17:50:07 +0530

    add build folder for dummy steps

---
+++ outoftree-llvm/build/CMakeFiles/myoutoftreetool.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myoutoftreetool.dir/helloworld.cpp.o"
+  "CMakeFiles/myoutoftreetool.dir/helloworld.cpp.o.d"
+  "myoutoftreetool"
+  "myoutoftreetool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myoutoftreetool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
